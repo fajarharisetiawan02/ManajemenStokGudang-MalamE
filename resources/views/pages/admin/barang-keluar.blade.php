34a1ffc1dffc5e4f56061d33f3be495f7d93db1d
KQuinn30 <yodapratama30@gmail.com>
2026-05-03 15:52:34 +0700

    kiw
    benerin typo

--- resources/views/pages/admin/barang-keluar.blade.php
+++ resources/views/pages/admin/barang-keluar.blade.php
@@ -29,7 +29,7 @@
 
             <!-- TANGGAL -->
             <div>
-                <label class="text-sm font-semibold text-gray-600">Tanggal Masuk</label>
+                <label class="text-sm font-semibold text-gray-600">Tanggal Keluar</label>
                 <input type="date" name="tanggal"
                     class="w-full px-3 py-2 border border-gray-300 rounded-lg mt-1 focus:ring-2 focus:ring-blue-500">
             </div>
